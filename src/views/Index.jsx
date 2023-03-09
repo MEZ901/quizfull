a531250ea9acacc3e79fc85919488a9c84e6157f
issam <issammez44@gmail.com>
2023-03-09 15:24:22 +0100

    file structer

---
+++ src/views/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='main min-h-screen'>
+      <h1 className='text-white text-center text-4xl font-bold'>QuizFull</h1>
+    </div>
+  )
+}
+
+export default App

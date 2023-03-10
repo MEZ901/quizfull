c6f53d533eac436f53d732e8f0812278ff7d4277
issam <issammez44@gmail.com>
2023-03-10 22:15:40 +0100

    add the router

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import Index from './views/Index';
 import store from './app/store';
 import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import router from './router';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Index />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 )
